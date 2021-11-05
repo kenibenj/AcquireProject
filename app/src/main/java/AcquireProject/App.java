e8e35a8285f03a51e2023f161a595f374931c5d6
kenibenj <90481627+kenibenj@users.noreply.github.com>
2021-11-05 15:21:35 -0600

    Update App.java

--- app/src/main/java/AcquireProject/App.java
+++ app/src/main/java/AcquireProject/App.java
@@ -4,9 +4,6 @@
 package AcquireProject;
 
 public class App {
-    public String getGreeting() {
-        return "Hello World!";
-    }
 
     public static void main(String[] args) {
         System.out.println(new App().getGreeting());
