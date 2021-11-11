18205e7ce5812930ca7d6d89d6413fcc61b78e77
TotallyJuice <michaelcollier10@gmail.com>
2021-11-11 12:31:57 -0700

    added documentation to ui classes

--- app/src/main/java/UserInterface/ActionMenu.java
+++ app/src/main/java/UserInterface/ActionMenu.java
@@ -3,6 +3,13 @@ package UserInterface;
 import AcquireProject.Game;
 import javafx.scene.layout.VBox;
 
+/**
+ * an action menu is switched out to change the options that a user is given during each step of the turn cycle
+ *
+ * @author Michael Collier
+ *
+ * @since 1.0.0
+ */
 public abstract class ActionMenu {
 
     protected VBox menu;
