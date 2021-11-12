496dd580f0e4234ab8c9a166094d97bc958de96e
astralis327 <emilyelz606@gmail.com>
2021-11-12 13:05:36 -0700

    Made loader singleton

--- app/src/main/java/AcquireProject/Loader.java
+++ app/src/main/java/AcquireProject/Loader.java
@@ -2,6 +2,8 @@
  * @author Emily Elzinga
  * @version 0.1.0
  * @since 11/12/2021
+ * Reads from and writes to a text file containing all the game data.
+ * This class is singleton since there should only ever be one loader in the game.
  */
 
 package AcquireProject;
@@ -14,8 +16,32 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
+
 public class Loader {
-    public Game loadGame(){
+    private static Loader INSTANCE;
+
+    protected Loader getINSTANCE() {
+        return INSTANCE;
+    }
+
+    /**
+     * Constructor that forces singleton
+     */
+    public Loader (){
+        super();
+        synchronized(App.class){
+            if(INSTANCE != null) throw new UnsupportedOperationException(
+                    getClass() + " is singleton but constructor called more than once");
+            INSTANCE = this;
+        }
+    }
+
+
+    /**
+     * Loads a game object from a text file.
+     * @return game object
+     */
+    private Game loadGame(){
         Game savedGame = null;
         try {
             String json = String.join("\n", Files.readAllLines(Paths.get("gameData.txt")));
@@ -28,6 +54,11 @@ public class Loader {
         return savedGame;
     }
 
+    /**
+     * Serializes a game object and saves it to a text file.
+     * @param gameData game object
+     * @throws IOException in case the file is not found or something is wrong with the data
+     */
     public void saveGame(Game gameData) throws IOException {
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
         Writer writer = new BufferedWriter((new OutputStreamWriter(
